b03d2f13f78a66026d6e90916834ca81fea296a8
James-S-007 <jspringer8@gatech.edu>
2021-10-04 00:12:15 -0400

    Updated for correct hardware and pinouts

--- main/main.cc
+++ main/main.cc
@@ -13,19 +13,29 @@
 //                3 October 2021:     Arduino Implementation
 //-----------------------------------------------------
 
-
-#include <stdio.h>
 #include <math.h>
 #include <Servo.h>
+#include <stdio.h>
 
 #define TempoCal 512
 #define TempoPotMax 1023
 #define PwmMax 255
 
-#define speakerPIN 5
-#define fanPIN 6
-#define syncLEDPIN 7
-#define playLEDPIN 8
+
+// pinouts
+#define SyncLED 7
+#define PlayLED 6
+#define TempoPot 14
+#define OctaveSelectPot 15
+#define ModeSelect 5
+#define Fan 9
+#define StartPB 3
+#define ResetPB 4
+#define Speaker 10
+// #define Mic A3
+// #define Servo 11
+//NOTE: Will use SCL/SDA pins for servo driver in actual circuit
+
 
 Servo strumServo;
 Servo fretServo;
@@ -33,10 +43,11 @@ Servo fretServo;
 int mode;
 int octave;
 int tempo;
+int duration;
 
 int songLength = 54;
 int beats[] = {2,1,2,1,2,1,1,1,2,1,2,1,1,1,2,1,1,1,6,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,1,2,1,1,1,5,1};
-songTempo = 250;
+int songTempo = 250;
 
 // set high and low servo values
 int strumLow = 50;
@@ -48,30 +59,32 @@ int fretHigh = 100;
 // called once on startup
 void setup() {
 
-    // set up inputs
-    pinMode(A0, INPUT);  // mode switch
-    pinMode(A1, INPUT);  // tempo potentiometer
-    pinMode(A2, INPUT);  // octave potentiometer
-    pinMode(A3, INPUT);  // start/stop button
-    pinMode(A4, INPUT);  // microphone
-
+    // set up inputs	
+	pinMode(TempoPot, INPUT);
+	pinMode(OctaveSelectPot, INPUT);
+	pinMode(ModeSelect, INPUT);
+	pinMode(StartPB, INPUT);
+	pinMode(ResetPB, INPUT);
+    // pinMode(Mic, INPUT);
+  
     // set up outputs
-    pinMode(speakerPIN, OUTPUT);
-    pinMode(fanPIN, OUTPUT);
-    pinMode(syncLEDPIN, OUTPUT);
-    pinMode(playLEDPIN, OUTPUT);
+  	pinMode(SyncLED, OUTPUT);
+	pinMode(PlayLED, OUTPUT);
+	pinMode(Speaker, OUTPUT);
+    pinMode(Fan, OUTPUT);
+  	//pinMode(Servo, OUTPUT);
 
+   
     strumServo.attach(9);
     fretServo.attach(10);
 
     // set initial motor and servo conditions
-    digitalWrite(motorPIN, 0);
     strumServo.write(5);
     fretServo.write(5);
-
-    digitalWrite(syncLEDPIN, LOW);
-    digitalWrite(playLEDPIN, LOW);
-
+    digitalWrite(Fan, LOW);
+  	digitalWrite(Speaker, LOW);
+    digitalWrite(SyncLED, LOW);
+    digitalWrite(PlayLED, LOW);
 }
 
 
@@ -79,14 +92,14 @@ void setup() {
 void loop() {
 
     // wait for start button to be pressed
-    while (!digitalRead(A3)) {}
+    while (!digitalRead(StartPB)) {}
 
     readOctave();
 
     // read mode switch
-    mode = digitalRead(A0);
+    mode = digitalRead(ModeSelect);
 
-    digitalWrite(syncLEDPIN, HIGH);
+    digitalWrite(SyncLED, HIGH);
 
     // execute code for current mode
     if (mode == 0) {  // play mode
@@ -94,12 +107,11 @@ void loop() {
         timingSync();
     } else {  // test mode
         // read tempo potentiometer and set tempo
-        tempoPot = analogRead(A1);
+        int tempoPot = analogRead(TempoPot);
         tempo = songTempo * float(tempoPot) / TempoCal;
     }
 
-    playSong();
-
+    playSong(tempo);
 }
 
 
@@ -111,23 +123,26 @@ void readOctave() {
 
 void tempoSync() {
     // tempo sync
+  	digitalWrite(SyncLED, HIGH);
     tempo = 200;
 }
 
 
 void timingSync() {
     // timing sync
+    digitalWrite(SyncLED, HIGH);
+    delay(2000);
+  
 }
 
 
 void playSong(int tempo) {
-
     // calculate note values based on octave
     int C = 16.3516 * pow(2, octave);
-    int D = 18.35405 * pow(2, octave;
-    int E = 20.60172 * pow(2, octave;
-    int F = 21.82676 * pow(2, octave;
-    int G = 24.49971 * pow(2, octave;
+    int D = 18.35405 * pow(2, octave);
+    int E = 20.60172 * pow(2, octave);
+    int F = 21.82676 * pow(2, octave);
+    int G = 24.49971 * pow(2, octave);
     int A = 27.5 * pow(2, octave);
     int B = 30.86771 * pow(2, octave);
     int high_C = 32.70320 * pow(2, octave);
@@ -135,31 +150,32 @@ void playSong(int tempo) {
 
     int notes[] = {C, rest, C, rest, C, rest, D, rest, E, rest, E, rest, D, rest, E, rest, F, rest, G, rest, high_C, rest, high_C, rest, high_C, rest, G, rest, G, rest, G, rest, E, rest, E, rest, E, rest, C, rest, C, rest, C, rest, G, rest, F, rest, E, rest, D, rest, C, rest};
 
-    digitalWrite(syncLEDPIN, LOW);
-    digitalWrite(playLEDPIN, HIGH);
+    digitalWrite(SyncLED, LOW);
+    digitalWrite(PlayLED, HIGH);
 
     // play song until stop button is pressed
     int i = 0;
-    while (!digitalRead(A3)) {
+    while (!digitalRead(ResetPB)) {
         // play current note
         duration = beats[i] * tempo;
-        tone(speakerPIN, notes[i], duration);
+        tone(Speaker, notes[i], duration);
         delay(duration);
 
         // move servos to opposite position
         moveServos();
 
         // increment index
-        i++;
+        ++i;
         if (i >= songLength) {
             i = 0;
         }
     }
 
     // reset fan and LEDs
-    noTone(fanPIN);
-    digitalWrite(syncLEDPIN, LOW);
-    digitalWrite(playLEDPIN, LOW);
+    noTone(Fan);
+    digitalWrite(Speaker, LOW);
+    digitalWrite(SyncLED, LOW);
+    digitalWrite(PlayLED, LOW);
         
 }
 
@@ -167,13 +183,13 @@ void playSong(int tempo) {
 void moveServos() {
     if (strumServo.read() == strumHigh) {
             strumServo.write(strumLow);
-        } else {
+    } else {
             strumServo.write(strumHigh);
-        }
+    }
 
-        if (fretServo.read() == fretHigh) {
-            fretServo.write(fretLow);
-        } else {
-            fretServo.write(fretHigh);
-        }
+    if (fretServo.read() == fretHigh) {
+        fretServo.write(fretLow);
+    } else {
+        fretServo.write(fretHigh);
+    }
 }
